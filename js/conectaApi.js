87020a6659c9c6ea0c5ac20af3e4588f26b75eb7
karinepa7 <karinepereira.a@hotmail.com>
2024-06-22 21:29:21 -0300

    second commit

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,36 +4,37 @@ async function listaVideos() {
   return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch("http://localhost:3000/videos", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+
+  if (!conexao.ok) {
+    throw new Error("Não foi possível enviar o vídeo.");
+  }
+
+  const conexaoConvertida = await conexao.json();
+  return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+  const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+  const conexaoConvertida = conexao.json();
+
+  return conexaoConvertida;
+}
+
 export const conectaApi = {
   listaVideos,
+  criaVideo,
+  buscaVideo,
 };
-
-const lista = document.querySelector("[data-lista]"); //data attributes
-function constroiCard(titulo, descricao, url, imagem) {
-  const video = document.createElement("li");
-  video.className = "videos__item";
-  video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
-                title="${titulo}" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="${descricao}">
-                <img src="${imagem}" alt="logo canal alura">
-                <h3>${titulo}</h3>
-                <p>${descricao}</p>`;
-  return video;
-}
-
-async function listaVideos() {
-  const listaApi = await conectaApi.listaVideos();
-  listaApi.forEach((elemento) =>
-    lista.appendChild(
-      constroiCard(
-        elemento.titulo,
-        elemento.descricao,
-        elemento.url,
-        elemento.imagem
-      )
-    )
-  );
-}
-listaVideos();
